41b9636f020b44548dc9619ab4177ceb343ffcfc
adlwar137 <adlerwarmbier@gmail.com>
2024-07-24 01:52:05 -0400

    main loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::collections::HashMap;
+use std::io;
 
 pub mod parser;
 
@@ -18,13 +19,57 @@ struct MediaEntry {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    //TODO check arguments to load a file
 
-    let query = &args[1];
-    let file_path = &args[2];
 
-    dbg!(load_file(file_path.to_string()));
+    let mut entries: Vec<MediaEntry> = Vec::new();
+    
+    loop {
+        let reply = ask("A) To add an entry");
 
+        match reply.to_lowercase().as_str() {
+            "a" => add_entry(&mut entries),
+            "m" => println!("modify..."),
+            "s" => println!("saving..."),
+            "l" => println!("loading..."),
+            _ => println!("whar??? in: ({reply})"),
+        };
+
+        dbg!(&entries);
+    }
+}
+
+fn ask(query: &str) -> String {
+    println!("{}", query);
+
+    let mut answer = String::new();
+
+    io::stdin()
+        .read_line(&mut answer)
+        .expect("Failed to read line");
+
+    answer.trim().to_string()
+}
+
+fn add_entry(entryList: &mut Vec<MediaEntry>) {
+    let name = ask("What is the title?");
+
+    let media = ask("What is the type of media?").to_lowercase();
+
+    let entry = MediaEntry {
+        mediaType: match media.as_str() {
+            "book" => MediaType::Book,
+            "movie" => MediaType::Movie,
+            "show" => MediaType::Show,
+            _ => {
+                println!("invalid media type");
+                return;
+            },
+        },
+        name
+    };
+
+    entryList.push(entry);
 }
 
 fn load_file(file_path: String) -> MediaEntry {
